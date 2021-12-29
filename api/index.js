ad4b52948a98320747e45edfa302c2b8d882523e
ercooss <95519428+ercooss@users.noreply.github.com>
2021-12-29 19:57:03 +0700

    re

--- api/index.js
+++ api/index.js
@@ -38,36 +38,8 @@ bot.on('message',(msg) =>{
 [
     parseFloat(s[0]), // string to float
     parseFloat(s[1])
-]
-   
-).then((jres1)=>{
-console.log(jres1);
-         
-    cls_model.classify([parseFloat(s[0]), parseFloat(s[1]), parseFloat(jres1[0]), parseFloat (jres1[1])]).then((jres2) => {
-    bot.sendMessage(
-         msg.chat.id,
-         `nilai yang diprediksi adalah ${jres1[0]} volt`
-
-);
-    bot.sendMessage(
-         msg.chat.id,
-           `nilai p yang diprediksi adalah ${jres1[1]} watt`
-);
-    bot.sendMessage(
-          msg.chat.id,
-            `Klasifikasi Tegangan ${jres2}`
-            );
-        state = 0;
-        })
-   })
-}else{
-    bot.sendMessage(
-         msg.chat.id,
-         `Please Click /start`
-    );
-state = 0;
-    }
-})
+])
+}});
 
 // routers
 r.get('/predict/:i/:r', function(req, res, next) {    
@@ -88,17 +60,6 @@ r.get('/Classify/:i/:r', function(req, res, next) {
             parseFloat(req.params.i), // string to float
             parseFloat(req.params.r)
         ] 
-    ).then((jres)=>{
-        cls_model.classify(
-            [
-            parseFloat(req.params.i), // string to float
-            parseFloat(req.params.r),
-            parseFloat(jres[0]), 
-            parseFloat(jres[1])
-            ]
-      ).then((jres_)=>{
-         res.json({jres,jres_})  
-            })
-        })
+    )
 });
 module.exports = r;

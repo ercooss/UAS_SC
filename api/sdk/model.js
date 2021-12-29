6544b11aaff2920603ecb3f2fd78801ad670be55
ercooss <95519428+ercooss@users.noreply.github.com>
2021-12-30 00:42:37 +0700

    Update model.js

--- api/sdk/model.js
+++ api/sdk/model.js
@@ -1,22 +1,38 @@
 const tf = require('@tensorflow/tfjs-node');
 
-function normalized(data){ // x1,x2 & x3
-    x1 = (data[0] -  42.794) / 10.034
+function normalized(data){ // i & r
+    x1 = (data[0] - 42.794) / 10.60339549
     x2 = (data[1] - 88.509) / 19.06251028
     x3 = (data[2] - 143.127) / 22.86503183
-    return [x1,x2,x3]
-}
-
-function denormalized(data){
-    y1 = (data[0] * 9.20165319) + 74.807
-    y2 = (data[1] * 49.766) + 14.85172777
-    y3 = (data[3] * 160.133) + 23.85217469
-    return [y1,y2,y3]
+    y1 = (data[3] - 74.807) / 9.20165319
+    y2 = (data[4] - 49.766) / 14.85172777
+    y3 = (data[5] - 160.133) / 23.85217469
+    return [x1,x2,x3,y1,y2,y3]
 }
 
 
-async function predict(data){
-    let in_dim = 3;
+const argFact = (compareFn) => (array) => array.map((el,idx) => [el,idx]).reduce(compareFn)[1]
+const argMax  = argFact((min,el) => (el[0] > min[0] ? el : min))
+
+function ArgMax(res){
+  label = "NORMAL"
+  cls_data = []
+  for(x1=0; x1<res.length; x1++){
+    cls_data [x1] = res[x1]
+  }
+  console.log(cls_data , argMax(cls_data));
+  
+  if(argMax(cls_data) == 1){
+    label = "OVER"
+  }if(argMax(cls_data)==0){
+      label = "DROP"
+    }
+    return label
+  }
+
+
+async function classify(data){
+    let in_dim = 6; //i r v p
     
     data = normalized(data);
     shape = [1, in_dim];
@@ -32,7 +48,7 @@ async function predict(data){
                 tf_data
         );
         result = predict.dataSync();
-        return denormalized( result );
+        return ArgMax( result );
         
     }catch(e){
       console.log(e);
@@ -40,6 +56,5 @@ async function predict(data){
 }
 
 module.exports = {
-    predict: predict 
+    classify: classify 
 }
-  

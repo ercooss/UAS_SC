106edb37d8c91d423442b5b0be1843a515bdca0c
ercooss <95519428+ercooss@users.noreply.github.com>
2021-12-30 13:07:05 +0700

    Update model.js

--- api/sdk/model.js
+++ api/sdk/model.js
@@ -2,21 +2,19 @@ const tf = require('@tensorflow/tfjs-node');
 
 function normalized(data){ // x & y
      x1 = (data[0] - 42.794) / 10.60339549
-    x2 = (data[1] - 88.509) / 19.06251028
-    x3 = (data[2] - 143.127) / 22.86503183  
-    return [x1,x2,x3]
+    x2 = (data[1] - 88.509) / 19.06251028  
+    return [x1,x2]
 }
 
 function denormalized(data){
     y1 = (data[0] * 9.20165319) + 74.807
     y2 = (data[1] * 14.85172777) + 49.766
-    y3 = (data[2] * 23.85217469) + 160.133
-    return [y1,y1,y3]
+    return [y1,y2]
 }
 
 
 async function predict(data){
-    let in_dim = 3;
+    let in_dim = 2;
     
     data = normalized(data);
     shape = [1, in_dim];
